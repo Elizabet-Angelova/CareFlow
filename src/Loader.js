92eedbf76502fb579ef5fb7c639b15ac6904a361
unknown <elizabet.angelova.a20@learn.telerikacademy.com>
2020-10-27 10:15:44 +0200

    fix loader

--- src/Loader.js
+++ src/Loader.js
@@ -3,7 +3,8 @@ import React from 'react';
 const Loader = ({fadeout}) => {
     return (
         <div className={`form fade-out`} style={{ background: 'black', zIndex: '100' }} >
-                <div className='loader-text' style={{fontFamily: 'Onyx, serif', color: 'white', letterSpacing: '0.14em'}}>CareFlow</div>
+            <img className='loader-image' src='/careflow-white-min.png' alt='logo'/>
+                {/* <div className='loader-text' style={{fontFamily: 'Onyx, serif', color: 'white', letterSpacing: '0.14em'}}>CareFlow</div> */}
             <div className='loader-circle'>
             </div>
         </div>
